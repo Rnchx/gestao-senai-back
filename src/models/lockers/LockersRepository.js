e5cc9203b54b61896a8d01b20d7b13a500bf8e91
analiviamartins <anamartins@aluno.senai.br>
2024-11-18 15:06:37 -0300

    rodar o servidor

--- src/models/lockers/LockersRepository.js
+++ src/models/lockers/LockersRepository.js
@@ -5,6 +5,78 @@ export default class LockersRepository {
     this.db = db;
   }
 
+  async getLockers() {
+    try {
+      const allLockers = await this.db.manyOrNone("SELECT * FROM lockers");
+      return allLockers;
+    } catch (error) {
+      console.error("Falha ao tentar buscar os armários", error);
+      throw error;
+    }
+  }
+
+  async getLockersById(id) {
+    try {
+      const locker = await this.db.oneOrNone("SELECT * FROM lockers WHERE id = $1", id);
+      return locker;
+    } catch (error) {
+      console.error(`Falha ao tentar buscar o armário ${id}`, error);
+      throw error;
+    }
+  }
+
+  async getLockersByOccupation(occupationStatus) {
+    try {
+      const locker = await this.db.manyOrNone("SELECT * FROM lockers WHERE occupationStatus = $1", occupationStatus);
+      return locker;
+    } catch (error) {
+      console.error(`Falha ao tentar descobrir a ocupação ${occupationStatus}`, error);
+      throw error;
+    }
+  }
+
+  async createLocker(locker) {
+    try {
+      await this.db.none(
+            "INSERT INTO lockers (occupationStatus, owner) VALUES ($1, $2)",
+            [locker.occupationStatus, locker.owner]
+      );
+      return locker
+    } catch (error) {
+      console.error(`Falha ao tentar cadastrar um armário`, error);
+        throw error;
+    }
+  }
+
+    async updateLoker(id, occupationStatus, owner) {
+    try {
+      const Locker = await this.getLockersById(id);
+
+      if (!Locker) {
+        return null;
+      }
+
+      const updatedLocker = await this.db.one(
+        "UPDATE lockers SET occupationStatus = $1, owner = $2 WHERE id = $3 RETURNING *",
+        [occupationStatus, owner, id]
+      );
+
+      return updatedLocker;
+    } catch (error) {
+      console.error(`Falha ao tentar atualizar armário${id}:`, error);
+      throw error;
+    }
+  }
+
+  async deleteLoker(id) {
+    try {
+      await this.db.none("DELETE FROM lockers WHERE id = $1", id);
+    } catch (error) {
+      console.error(`Falha ao tentar deletar o armário ${id}:`, error);
+      throw error;
+    }
+  }
+
   async assignStudentToLocker(id, studentId) {
     try {
       // Atribui o aluno ao armário, usando o ID do aluno como referência
